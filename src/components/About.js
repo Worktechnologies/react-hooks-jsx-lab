d8047f7fe827fdca84c63e1096bbecfa52c9a5fd
Parmjeet Kaur <work.technologies25@gmail.com>
2023-03-02 19:07:19 +1000

    done!

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+  <h2>About me</h2>
+  <p>Learning to use the imported variables into JSX elements</p>
+   <img src={ image } alt="I made this"></img>
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="#home">Home
+  <h1 style color="firebrick">"Your Name is a Web Developer from Your City"</h1>
+  <name/>
+  <city/>
+  </div>;
+  
 }
 
 export default Home;
